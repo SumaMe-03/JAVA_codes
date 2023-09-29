0584a2a4ba4250cf118946264ce9ca6fad35dddd
SumaMe-03 <108144650+SumaMe-03@users.noreply.github.com>
2023-09-29 18:39:04 +0530

    contest 3_3

---
+++ contest/contest3_3.java
@@ -0,0 +1,19 @@
+//Given a string s, return the string after replacing every upper case letter with the same lowercase letter. 
+//Input: s="Hello Student How is Going on Now" Output:"hello student how is going on now"
+
+import java.util.*;
+public class contest3_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = new String();
+        Scanner obj = new Scanner(System.in);
+        
+        str = obj.nextLine();
+        
+        str = str.toLowerCase();
+        System.out.println(str);
+
+	}
+
+}
